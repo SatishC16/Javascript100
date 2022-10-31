927ab0d9fe4ac8fed38af43f0085171976a96b09
SatishC16 <satishchandan786@gmail.com>
2022-10-31 23:22:51 +0530

    Day-1 Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Javascript100
 100 days of JavaScript
+
+# Day-1
+
+*Adrian Hajdin - Complete Path to JavaScript Mastery
+1. Local Environment Setup
+2. Variables
+3. Datatypes
+4. Comments
--- intro/script.js
+++ intro/script.js
@@ -1,3 +1,35 @@
 console.log("Hello JavaScript");
 console.log("Test");
 console.log("Reload LiveServer Test");
+
+console.log("Welcome to variables");
+
+var variableName = "Welcome to variables stored";
+console.log(variableName);
+
+let variableName1 = "Welcome to variables stored let";
+console.log(variableName1);
+
+const variableName2 = "Welcome to variables stored const";
+console.log(variableName2);
+
+variableName = "var";
+variableName1 = "let";
+// variableName2 = "const";
+
+console.log(variableName);
+console.log(variableName1);
+console.log(variableName2);
+
+// This is single-line comment
+
+/* This is multi-line comment
+    Datatypes
+    1. String
+    2. Numbers
+    3. Booleans
+    4. Null
+    5. Undefined
+    6. Symbol
+    7. Object
+*/
